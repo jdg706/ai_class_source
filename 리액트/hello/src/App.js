15b640a3860f9f660ffafaf438230ac4536023ce
jdg706 <jdg706@naver.com>
2025-05-09 17:28:43 +0900

    .

---
+++ 리액트/hello/src/App.js
@@ -0,0 +1,13 @@
+import Mycomponent from "./Mycomponent";
+import Mycomponent2 from "./Mycomponent2";
+
+function App() {
+  return (
+    <>
+      <Mycomponent/>
+      <Mycomponent2/>
+    </>
+  )
+}
+
+export default App;
---
+++ 리액트/hello/src/Mycomponent2.js
@@ -0,0 +1,7 @@
+function Mycomponent2() {
+    return (
+        <h1>나의 리액트 컴포넌트2!</h1>
+    )
+}
+
+export default Mycomponent2;
